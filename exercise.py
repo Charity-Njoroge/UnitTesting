df5e4ce48224d89fc20cee7d6d2cbe83c74dd28a
Charity Njoroge <njorogecharity1@gmail.com>
2018-10-11 09:58:48 +0300

    changes made to the test file

--- exercise.py
+++ exercise.py
@@ -9,19 +9,25 @@
 """
 import sys
 
+
 class book_rental:
     def __init__(self, name, id):
         self.name = customer_name
         self.id = cust_id
 
     def total_rental_cost1():
-        customer_name= input ("Enter your names ")
-        cust_id = input ( "Enter your library customer ID")
-        no_of_books_rent = input
-        no_of_days_rented = int(input ("enter the total"
-            "number of days the book was rented"))
-        cost_per_Day = 1
-        total_cost = no_of_days_rented * \
+        customer_name = input("Enter your names ")
+        cust_id = input("Enter your library customer ID")
+        no_of_books_rented = int(input("enter the total"
+                                      "number of books that"
+                                      "were rented"))
+        no_of_days_rented = int(input("enter the total"
+                                      "number of days the"
+                                      "book was rented"))
+        cost_per_day = 1
+        total_cost_per_day = cost_per_day * \
             no_of_books_rented
-    
+        total_cost = no_of_days_rented * \
+            total_cost_per_day
+
         return customer_name, total_cost
