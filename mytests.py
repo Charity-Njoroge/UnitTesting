b067c14ff4287a127da3bbcc7b0fb2cb6534beaa
Charity Njoroge <njorogecharity1@gmail.com>
2018-10-11 07:04:21 +0300

    changes made to the test file

--- mytests.py
+++ mytests.py
@@ -1,28 +1,46 @@
 import unittest
  
-class FooTest(unittest.TestCase):
+class book_rental_charge(unittest.TestCase):
     """Sample test case"""
      
     # preparing to test
     def setUp(self):
         """ Setting up for the test """
-        print "FooTest:setUp_:begin"
-        ## do something...
-        print "FooTest:setUp_:end"
+        print "book_rental_charge:setUp_:begin"
+         
+        testName = self.shortDescription()
+        if (testName == "total_rental_cost1"):
+            print "setting up for total_rental_cost1"
+             
+        elif (testName == "Test routine B"):
+            print "setting up for test B"
+             
+        else:
+            print "UNKNOWN TEST ROUTINE"
+             
+        print "book_rental_charge:setUp_:end"
      
     # ending the test
     def tearDown(self):
         """Cleaning up after the test"""
-        print "FooTest:tearDown_:begin"
-        ## do something...
-        print "FooTest:tearDown_:end"
+        print "book_rental_charge:tearDown_:begin"
+         testName = self.shortDescription()
+        if (testName == "total_rental_cost1"):
+            print "cleaning up after total_rental_cost1"
+             
+        elif (testName == "Test routine B"):
+            print "cleaning up after test B"
+             
+        else:
+            print "UNKNOWN TEST ROUTINE"
+        print "book_rental_charge:tearDown_:end"
      
     # test routine A
-    def testA(self):
+    def total_rental_cost1(self):
         """Test routine A"""
-        print "FooTest:testA"
+        print "book_rental_charge:testA"
      
     # test routine B
     def testB(self):
         """Test routine B"""
-        print "FooTest:testB"
+        print "book_rental_charge:testB"
